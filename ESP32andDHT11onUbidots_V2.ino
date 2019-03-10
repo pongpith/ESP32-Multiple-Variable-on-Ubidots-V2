16bc0a659209257e5ff467c9a15179c9c60ce0d2
Pongpith Simmanee <pongpith.cy@gmail.com>
2019-03-10 16:03:17 +0700

    initial

---
+++ ESP32andDHT11onUbidots_V2.ino
@@ -0,0 +1,66 @@
+
+/****************************************
+ * Include Libraries
+ ****************************************/
+ 
+#include "WiFi.h"
+#include "UbidotsMicroESP32.h"
+#include <Wire.h>
+#include <Arduino.h>
+#include "DHTesp.h"
+
+
+/****************************************
+ * Define Constants
+ ****************************************/
+
+#define DHTpin 4    //ขา DHT11
+#define TOKEN  "A1E-QICFYVfwAheWmTBjHF4WDnQHFA5e6w"  // Put here your Ubidots TOKEN
+#define WIFISSID "iotworkshop" // Put here your Wi-Fi SSID
+#define PASSWORD "123414321*" // Put here your Wi-Fi password
+
+
+/****************************************
+ * Used Lib
+ ****************************************/
+DHTesp dht;
+Ubidots client(TOKEN);
+
+/****************************************
+ * Golbal Val
+ ****************************************/
+String getStatusDHT11;
+
+void setup() {
+  Serial.begin(115200);
+  dht.setup(DHTpin, DHTesp::DHT11); //for DHT11 Connect DHT sensor 
+  delay(100);
+  client.wifiConnection(WIFISSID,PASSWORD);
+  client.setDataSourceName("My test"); // ชื่อ Device
+  client.setDataSourceLabel("my-test"); // ชื่อ variable ใน Device
+   
+/* ****************************** END *********************** */ 
+ 
+}
+
+void loop() {
+
+  
+  /* ********************* initial DHT11 ********************* */ 
+  
+  delay(dht.getMinimumSamplingPeriod());
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+  getStatusDHT11 = String(dht.getStatusString());
+  
+   /* ****************************** END *********************** */     
+
+// ตั้งเงื่อนไขว่า ถ้า Status ของ Sensor DHT11 พร้่อมทำงาน คือคำว่า OK จะส่งข้อมูล
+  if(getStatusDHT11 == "OK") { 
+    client.add("Humidity", humidity);
+    client.add("Temperature", temperature);
+    client.sendAll(true); 
+    }
+
+ /* ****************************** END *********************** */     
+}
